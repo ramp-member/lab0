b50e9f766d6095fadff6a4c26f9f7f252475f3b6
ramp-member <v279136@gmail.com>
2025-09-09 23:50:05 +0300

    lab 1 дз

---
+++ dz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void house() {
+    puts("       /\\\\\\\\     ");
+    puts("      /  \\\\\\\\    ");
+    puts("     /    \\\\\\\\   ");
+    puts("    /______\\\\\\\\  ");
+    puts("   |        |  |     ");
+    puts("   |        |--|     ");
+    puts("   |   __   |--|     ");
+    puts("   |  |  |  |  |     ");
+    puts("   |__|__|__|__|     ");
+}
+
+int main() {
+    house();
+    return 0;
+}
